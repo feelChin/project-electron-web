4c2dc75c049b3fb4350717f9e6eb1ec9aea97d4f
秦记 <4584@qq.com>
2024-03-27 13:48:42 +0800

    新增demo

---
+++ assets/index-fWcOb7iY.js
@@ -0,0 +1,151 @@
+import { r as reactExports, _ as __vitePreload, R as React } from "./index-jLRFKII-.js";
+import "./index-2YrWd7R0.js";
+import { J, U as UserContext, i as it } from "./useContext-9zwRh2Kj.js";
+const exampleList$1 = {
+  drag: reactExports.lazy(() => __vitePreload(() => import("./index-OMu4Bk-8.js"), true ? __vite__mapDeps([0,1,2,3]) : void 0, import.meta.url)),
+  timeCountDown: reactExports.lazy(() => __vitePreload(() => import("./index-xeWSM7O4.js"), true ? __vite__mapDeps([4,1,2,5]) : void 0, import.meta.url)),
+  verificationCode: reactExports.lazy(() => __vitePreload(() => import("./index-NUeZSGAp.js"), true ? __vite__mapDeps([6,1,2,7]) : void 0, import.meta.url)),
+  intersectionObserverScrollList: reactExports.lazy(() => __vitePreload(() => import("./index-5A_KJ9Jv.js"), true ? __vite__mapDeps([8,1,2,9]) : void 0, import.meta.url)),
+  websocket: reactExports.lazy(() => __vitePreload(() => import("./index-Cd4VLY-8.js"), true ? __vite__mapDeps([10,1,2,11]) : void 0, import.meta.url)),
+  message: reactExports.lazy(() => __vitePreload(() => import("./index-YxfUF4LY.js"), true ? __vite__mapDeps([12,1,2,13]) : void 0, import.meta.url)),
+  plateKeyboard: reactExports.lazy(() => __vitePreload(() => import("./index-gTY7IsnC.js"), true ? __vite__mapDeps([14,1,2,15]) : void 0, import.meta.url)),
+  theQuest: reactExports.lazy(() => __vitePreload(() => import("./index-qvXKrP0m.js"), true ? __vite__mapDeps([16,1,2,17]) : void 0, import.meta.url)),
+  tableAddAndRemove: reactExports.lazy(() => __vitePreload(() => import("./index-e52baWSS.js"), true ? __vite__mapDeps([18,1,2,19]) : void 0, import.meta.url)),
+  shoppingCart: reactExports.lazy(() => __vitePreload(() => import("./index-MAC0pfgy.js"), true ? __vite__mapDeps([20,1,2,21,22]) : void 0, import.meta.url)),
+  tree: reactExports.lazy(() => __vitePreload(() => import("./index-ffagQFNc.js"), true ? __vite__mapDeps([23,1,2,24,25]) : void 0, import.meta.url)),
+  myModal: reactExports.lazy(() => __vitePreload(() => import("./index-btQNapMM.js"), true ? __vite__mapDeps([26,1,2,27]) : void 0, import.meta.url)),
+  accordion: reactExports.lazy(() => __vitePreload(() => import("./index-JnwBT1DG.js"), true ? __vite__mapDeps([28,1,2,29]) : void 0, import.meta.url)),
+  entrust: reactExports.lazy(() => __vitePreload(() => import("./index-_LmGa1pi.js"), true ? __vite__mapDeps([30,1,2,31]) : void 0, import.meta.url)),
+  splitText: reactExports.lazy(() => __vitePreload(() => import("./index-wTAUfi2D.js"), true ? __vite__mapDeps([32,1,2,33]) : void 0, import.meta.url)),
+  fixedElement: reactExports.lazy(() => __vitePreload(() => import("./index-S0jeTsev.js"), true ? __vite__mapDeps([34,1,2,35]) : void 0, import.meta.url)),
+  followArrow: reactExports.lazy(() => __vitePreload(() => import("./index-VcoxTgWX.js"), true ? __vite__mapDeps([36,1,2,37]) : void 0, import.meta.url)),
+  loadingImg: reactExports.lazy(() => __vitePreload(() => import("./index-5nAAzYsv.js"), true ? __vite__mapDeps([38,1,2,39]) : void 0, import.meta.url)),
+  mouseDocumengtMove: reactExports.lazy(() => __vitePreload(() => import("./index-aIQ8SXsd.js"), true ? __vite__mapDeps([40,1,2,41]) : void 0, import.meta.url)),
+  scrollPageFollowAnimation: reactExports.lazy(() => __vitePreload(() => import("./index-UztGceZV.js"), true ? __vite__mapDeps([42,1,2,43]) : void 0, import.meta.url)),
+  scrollPageTransfrom: reactExports.lazy(() => __vitePreload(() => import("./index-Ip8I9q78.js"), true ? __vite__mapDeps([44,1,2,45]) : void 0, import.meta.url)),
+  scrollPageTransfromHorizontally: reactExports.lazy(() => __vitePreload(() => import("./index-P4IHP9ed.js"), true ? __vite__mapDeps([46,1,2,47]) : void 0, import.meta.url)),
+  shell: reactExports.lazy(() => __vitePreload(() => import("./index-OpIvQDUM.js"), true ? __vite__mapDeps([48,1,2,49]) : void 0, import.meta.url)),
+  tilt: reactExports.lazy(() => __vitePreload(() => import("./index-frqQmb-e.js"), true ? __vite__mapDeps([50,1,2,51]) : void 0, import.meta.url)),
+  mask: reactExports.lazy(() => __vitePreload(() => import("./index-AiibbvAA.js"), true ? __vite__mapDeps([52,1,2,53]) : void 0, import.meta.url)),
+  scrollpageActive: reactExports.lazy(() => __vitePreload(() => import("./index-E2cVOF7b.js"), true ? __vite__mapDeps([54,1,2,55]) : void 0, import.meta.url)),
+  imgToVideo: reactExports.lazy(() => __vitePreload(() => import("./index-WtsIvF3d.js"), true ? __vite__mapDeps([56,1,2,57]) : void 0, import.meta.url)),
+  mouseFollow: reactExports.lazy(() => __vitePreload(() => import("./index-VI0vm7SY.js"), true ? __vite__mapDeps([58,1,2,59]) : void 0, import.meta.url)),
+  music: reactExports.lazy(() => __vitePreload(() => import("./index-gAPuZWnn.js"), true ? __vite__mapDeps([60,1,2,21,61]) : void 0, import.meta.url)),
+  custonScrollTheme: reactExports.lazy(() => __vitePreload(() => import("./index-eJg2juXo.js"), true ? __vite__mapDeps([62,1,2,63,64]) : void 0, import.meta.url)),
+  virtualList: reactExports.lazy(() => __vitePreload(() => import("./index-7DrNAiDT.js"), true ? __vite__mapDeps([65,1,2,63,64,66]) : void 0, import.meta.url)),
+  virtualLoop: reactExports.lazy(() => __vitePreload(() => import("./index-YF05d5l4.js"), true ? __vite__mapDeps([67,1,2,68]) : void 0, import.meta.url)),
+  noticeLoopScroll: reactExports.lazy(() => __vitePreload(() => import("./index-2JPEKdEK.js"), true ? __vite__mapDeps([69,1,2,70,24,71]) : void 0, import.meta.url)),
+  customRoute: reactExports.lazy(() => __vitePreload(() => import("./index-7T9V2TcX.js"), true ? __vite__mapDeps([72,1,2,70,24,73]) : void 0, import.meta.url)),
+  promiseLimit: reactExports.lazy(() => __vitePreload(() => import("./index-phMZNODi.js"), true ? __vite__mapDeps([74,1,2,75]) : void 0, import.meta.url)),
+  numberAni: reactExports.lazy(() => __vitePreload(() => import("./index-RrA1YJv_.js"), true ? __vite__mapDeps([76,1,2,77]) : void 0, import.meta.url))
+};
+const example = "_example_1pkhw_1";
+const exampleList = "_exampleList_1pkhw_7";
+const exampleListWrapper = "_exampleListWrapper_1pkhw_14";
+const item = "_item_1pkhw_21";
+const title = "_title_1pkhw_54";
+const text = "_text_1pkhw_73";
+const exampleModal = "_exampleModal_1pkhw_86";
+const exampleModalWrapper = "_exampleModalWrapper_1pkhw_95";
+const content = "_content_1pkhw_118";
+const style = {
+  example,
+  exampleList,
+  exampleListWrapper,
+  item,
+  title,
+  text,
+  exampleModal,
+  exampleModalWrapper,
+  content
+};
+function Index() {
+  const [props, api] = J(() => ({
+    from: {
+      opacity: 0,
+      x: "100%"
+    }
+  }), []);
+  const {
+    userInfo
+  } = UserContext();
+  const [exampleModal2, setExampleModal] = reactExports.useState({});
+  const [list] = reactExports.useState(userInfo?.example?.data);
+  function renderExample() {
+    return list.map(({
+      name,
+      text: text2,
+      url,
+      type
+    }) => /* @__PURE__ */ React.createElement("div", {
+      key: url,
+      className: `${style.item} ${style[type]}`,
+      onClick: () => {
+        setExampleModal({
+          name,
+          url
+        });
+        api.start({
+          from: {
+            x: "100%",
+            opacity: 0
+          },
+          to: {
+            x: "0",
+            opacity: 1
+          }
+        });
+      }
+    }, /* @__PURE__ */ React.createElement("div", {
+      className: style.title
+    }, /* @__PURE__ */ React.createElement("h5", null, name, " ."), /* @__PURE__ */ React.createElement("span", {
+      style: {
+        background: `var(--${type})`
+      }
+    }, type)), /* @__PURE__ */ React.createElement("div", {
+      className: style.text
+    }, text2)));
+  }
+  const LazyElement = exampleList$1[exampleModal2.url];
+  return /* @__PURE__ */ React.createElement("div", {
+    className: style.example
+  }, /* @__PURE__ */ React.createElement("div", {
+    className: style.exampleList
+  }, /* @__PURE__ */ React.createElement("div", {
+    className: style.exampleListWrapper
+  }, renderExample())), /* @__PURE__ */ React.createElement(it.div, {
+    style: props,
+    className: style.exampleModal
+  }, exampleModal2.url && /* @__PURE__ */ React.createElement("div", {
+    className: style.exampleModalWrapper
+  }, /* @__PURE__ */ React.createElement("div", {
+    className: style.title
+  }, /* @__PURE__ */ React.createElement("div", {
+    className: "iconArrow",
+    onClick: () => {
+      api.start({
+        from: {
+          x: "0",
+          opacity: 1
+        },
+        to: {
+          x: "100%",
+          opacity: 0
+        }
+      });
+    }
+  }), /* @__PURE__ */ React.createElement("h5", null, exampleModal2.name)), /* @__PURE__ */ React.createElement("div", {
+    className: style.content
+  }, /* @__PURE__ */ React.createElement(reactExports.Suspense, {
+    fallback: "渲染中"
+  }, /* @__PURE__ */ React.createElement(LazyElement, null))))));
+}
+export {
+  Index as default
+};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./index-OMu4Bk-8.js","./index-jLRFKII-.js","./index-tNx9U5HO.css","./index-Gcl5a4_d.css","./index-xeWSM7O4.js","./index-ai22FUNv.css","./index-NUeZSGAp.js","./index-agk5Iac4.css","./index-5A_KJ9Jv.js","./index-9Vd7UCmH.css","./index-Cd4VLY-8.js","./index-7o4_S-l1.css","./index-YxfUF4LY.js","./index-5nD7lf_h.css","./index-gTY7IsnC.js","./index-MJ7VcHM9.css","./index-qvXKrP0m.js","./index-HJ5oHjj9.css","./index-e52baWSS.js","./index-TcPKqTMX.css","./index-MAC0pfgy.js","./index-yqe05GmQ.js","./index-VYztutsh.css","./index-ffagQFNc.js","./pickAttrs-XbcIPBdU.js","./index-ZLieJ24n.css","./index-btQNapMM.js","./index-kiZue0hY.css","./index-JnwBT1DG.js","./index--GMC-Cta.css","./index-_LmGa1pi.js","./index-BJfZqlH5.css","./index-wTAUfi2D.js","./index-H9Me4D6r.css","./index-S0jeTsev.js","./index-Hk16I_8h.css","./index-VcoxTgWX.js","./index-RjmIcb6x.css","./index-5nAAzYsv.js","./index-6uMgo_6r.css","./index-aIQ8SXsd.js","./index-RhgPaIKC.css","./index-UztGceZV.js","./index-0QGCtfp0.css","./index-Ip8I9q78.js","./index-yYnPKNe7.css","./index-P4IHP9ed.js","./index-JigSvLQP.css","./index-OpIvQDUM.js","./index--AWb1bXW.css","./index-frqQmb-e.js","./index-uakyeSuq.css","./index-AiibbvAA.js","./index-QBfaPoKq.css","./index-E2cVOF7b.js","./index-23cabbL5.css","./index-WtsIvF3d.js","./index-VGIuhOyO.css","./index-VI0vm7SY.js","./index-KuKjsipQ.css","./index-gAPuZWnn.js","./index-wY68HC8T.css","./index-eJg2juXo.js","./custonScrollTheme-VcLGSEX5.js","./custonScrollTheme-RgiT5IOJ.css","./index-7DrNAiDT.js","./index--o5iWh7N.css","./index-YF05d5l4.js","./index-12KiJ0C-.css","./index-2JPEKdEK.js","./row-Q0vgk8Cn.js","./index-IRh1F4mx.css","./index-7T9V2TcX.js","./index-EVEsDvV0.css","./index-phMZNODi.js","./index-1FOEBi7y.css","./index-RrA1YJv_.js","./index-Al4Qr5d1.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
